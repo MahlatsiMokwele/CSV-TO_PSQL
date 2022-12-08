025083fc26714a977a491b65c671c1a16419d308
Mahlatsi <mahlatsi@geoint.africa>
2022-12-08 22:19:18 +0200

    WIP added logging, added tests and added methods

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from config import config
 import os
 import csv
 import psycopg2
+import Logging
 
 # Env Sourcing
 load_dotenv('.env')
@@ -12,50 +13,41 @@ pg_user = os.envriron.get("POSTGRES_USER")
 csv_fil = os.envriron.get("CSV_LOCATION")
 pg_port = os.envriron.get("POSTGRES_PORT")
 pg_password = os.envriron.get("POSTGRES_PASSWORD")
+sql_file = os.envriron.get("SCHEMA_LOCATION")
 
-# Database Connection 
-db_connection = psycopg2.connect(
-    database=pg_db,
-    user=pg_user,
-    password=pg_password,
-    host=pg_host,
-    port=pg_port
-)
+def database_connection():
+# Database Connection
+    db_connection = None
+    try:
+        Logging.info("Connecting to the database...")
+        db_connection = psycopg2.connect(
+            database=pg_db,
+            user=pg_user,
+            password=pg_password,
+            host=pg_host,
+            port=pg_port
+        )
+    except (Exception, psycopg2.DatabaseError) as e:
+        Logging.error(e)
+    Logging.info("Database connection established")
+    return db_connection
 
 def create_tables():
     # Create tables in db
-    sql_query = (
-        """
-        CREATE SCHEMA IF NOT EXISTS road_segments;
-        """,
-        """
-        CREATE TABLE IF NOT EXISTS road_segments.202212000_road_segments as (
-            feat_id uuid,
-            geom public.geometry,
-            pos_azimuth double precision,
-            neg_azimuth double precision,
-            pos_passenger numeric,
-            pos_truck numeric,
-            pos_private_bus numeric,
-            pos_public_bus numeric,
-            pos_other numeric,
-            neg_passenger numeric,
-            neg_truck numeric,
-            neg_private_bus numeric,
-            neg_public_bus numeric,
-            neg_other numeric
-        ) 
-        """
-    )
+    db_connection = database_connection()
     try:
-        db_connection.autocommit = True
         cursor = db_connection.cursor()
-        cursor.execute(sql_query)
+        cursor.execute(open(sql_file, 'r').read())
+        db_connection.autocommit = True
     except (Exception, psycopg2.DatabaseError) as db_error:
-        print(db_error)
+        Logging.error(db_error)
+        cursor.rollback()
+    
+    Logging.info("Query Complete")
 
 def import_csv():
     # Read and import CSV file
+    db_connection = database_connection()
     with open(csv_fil, 'r') as file:
         csv_reader = csv.reader(file)
         next(csv_reader)
@@ -77,6 +69,11 @@ def import_csv():
                 if db_connection is not None:
                     db_connection.close()
 
-if __name__ == "__main__":
+def main():
+    db_connection = database_connection()
     create_tables()
     import_csv()
+    db_connection.close()
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test.py
@@ -0,0 +1,5 @@
+import pytest_
+from main import *
+
+def connection_test():
+    assert database_connection != None
